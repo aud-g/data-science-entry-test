5b6820ff0ebabe5f34ea0256dc167f9c22376154
aud-g <bansheeaud@gmail.com>
2025-04-04 09:42:01 +0800

    q4.py

---
+++ q4.py
@@ -0,0 +1,28 @@
+def string_reverse(s):
+    """
+    Task 1
+    - Create a function that reverses a given string (s).
+    - s must be a string.
+    - Return the reversed string.
+    """
+    return
+
+
+# Task 2
+# Invoke the function "string_reverse" using the following scenarios:
+# - "Hello World"
+# - "Python"
+"""
+
+Task 1 Ans
+if not isinstance(s, str):
+    return None # or raise an error.
+return s[::-1]
+
+Task 2 Ans
+result1 = string_reverse("Hello World")
+print(result1)
+
+result2 = string_reverse("Python")
+print(result2)
+"""
