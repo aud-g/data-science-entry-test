1e549b624e2b5a8a73653f2bdecd623c0a464e9b
aud-g <bansheeaud@gmail.com>
2025-04-04 21:26:38 +0800

    q7.py

--- q7.py
+++ q7.py
@@ -16,3 +16,16 @@ class Car:
 """
 
 Task 1 Ans
+def __init__(self, make, model, year):
+    self.make = make
+    self.model = model
+    self.year = year
+
+def describe_car(self):
+    print(f"{self.year} {self.make} {self.model}")
+
+Task 2 Ans
+my_car = Car("Toyota", "Corolla", 2020)
+my_car.describe_car()
+"""
+    
